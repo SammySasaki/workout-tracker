53c5bbcb62069092c8587c6a75933f5faa461f49
Sammy Sasaki <sasakisammy2@gmail.com>
2022-08-03 20:14:01 -0700

    fix authentication

--- backend/settings.py
+++ backend/settings.py
@@ -168,7 +168,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 25,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
